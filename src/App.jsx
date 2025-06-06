2f2a0550f9735c1f9119a830181ae1e65e525b74
Drillenissen <logicguy.mailandcontact@gmail.com>
2025-06-06 10:00:27 +0200

    Fixed browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from 'react';
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { CssBaseline, ThemeProvider, Box } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
 import './App.css';
@@ -27,47 +27,45 @@ const App = () => {
   const appRef = useRef(null);
 
   return (
-    <BrowserRouter basename="/bread-dough-calculator">
-      <HeadProvider value={{ data: headerData, setData: setHeaderData, expanded }}>
-        <SnackBarProvider value={{ text: snackText, setText: setSnackText }}>
-          <ColorModeContext.Provider value={colorMode}>
-            <ThemeProvider theme={theme}>
-              <CssBaseline />
-              <Box
-                className="App"
-                display="flex"
-                height="100%"
-                ref={appRef}
-              >
-                {/*<Sidebar setExpanded={setExpanded} />*/}
-                <Box>
-                  <Topbar drawerOpen={expanded} setDrawerOpen={setExpanded} />
-                  <Box component="main" sx={{
-                    flexGrow: 1,
-                    p: 3,
-                    mt: '52px', // Height of AppBar
-                    width: { sm: `calc(100vw - ${expanded ? '240px' : '0px'})` },
-                    ml: { sm:expanded ? '240px' : '0px' },
-                    transition: theme.transitions.create(['margin', 'width'], {
-                      easing: theme.transitions.easing.sharp,
-                      duration: theme.transitions.duration.leavingScreen,
-                    }),
-                  }}>
-                    <Routes>
-                      {/* Routes will be added here */}
-                      <Route path="/" element={<BreadDoughConfigurator />} />
+    <HeadProvider value={{ data: headerData, setData: setHeaderData, expanded }}>
+      <SnackBarProvider value={{ text: snackText, setText: setSnackText }}>
+        <ColorModeContext.Provider value={colorMode}>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Box
+              className="App"
+              display="flex"
+              height="100%"
+              ref={appRef}
+            >
+              {/*<Sidebar setExpanded={setExpanded} />*/}
+              <Box>
+                <Topbar drawerOpen={expanded} setDrawerOpen={setExpanded} />
+                <Box component="main" sx={{
+                  flexGrow: 1,
+                  p: 3,
+                  mt: '52px', // Height of AppBar
+                  width: { sm: `calc(100vw - ${expanded ? '240px' : '0px'})` },
+                  ml: { sm:expanded ? '240px' : '0px' },
+                  transition: theme.transitions.create(['margin', 'width'], {
+                    easing: theme.transitions.easing.sharp,
+                    duration: theme.transitions.duration.leavingScreen,
+                  }),
+                }}>
+                  <Routes>
+                    {/* Routes will be added here */}
+                    <Route path="/" element={<BreadDoughConfigurator />} />
 
-                      {/* 404 Page - This should be the last route */}
-                      <Route path="*" element={<NotFoundScene />} />
-                    </Routes>
-                  </Box>
+                    {/* 404 Page - This should be the last route */}
+                    <Route path="*" element={<NotFoundScene />} />
+                  </Routes>
                 </Box>
               </Box>
-            </ThemeProvider>
-          </ColorModeContext.Provider>
-        </SnackBarProvider>
-      </HeadProvider>
-    </BrowserRouter>
+            </Box>
+          </ThemeProvider>
+        </ColorModeContext.Provider>
+      </SnackBarProvider>
+    </HeadProvider>
   );
 };
 
